b15988372a999f1dc807b16e1fb7a280f0008a68
Brendan Warnick <warnickb@eos15.cis.gvsu.edu>
2018-03-23 13:58:11 -0400

    added style

--- src/Superhero.cpp
+++ src/Superhero.cpp
@@ -2,9 +2,11 @@
 
 #include <iostream>
 #include <string>
+/**
+ * Consturctor class for superheroes
+ */
 
-
-Superhero(int page_id, const std::string &name, const std::string &urlslug, const std::string &id, const std::string &alignment, char eye_color, char hair_color, char sex, const std::string &gsm, bool alive, int appearances, const std::string &first_apperance, int year){
+Superhero::Superhero(int page_id, const std::string &name, const std::string &urlslug, const std::string &id, const std::string &alignment, char eye_color, char hair_color, char sex, const std::string &gsm, bool alive, int appearances, const std::string &first_apperance, int year){
 	this->page_id = page_id;
 	this->name = name;
 	this->urlslug = urlslug;
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,20 @@
 
 #include <string>
 #include <iostream>
-
-nt main(int argc, char** argv){
+/**
+ * @Author Brendan Warnick
+ * @Version Winter 2018
+ * CIS 263
+ * main file
+ * Used sample code from class
+ */
+int main(int argc, char** argv){
         io::CSVReader<5, io::trim_chars<' '>, io::double_quote_escape<',','\"'> > in("marvel.csv");
         in.read_header(io::ignore_extra_column, "page_id", "name", "ALIGN", "EYE", "HAIR");
  
         std::vector<Superhero> heros;
  
-        int id;
+        int page_id;
         std::string name;
         std::string alignment;
         std::string eye;
